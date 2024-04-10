ec0576792a9c24347566749b2db7eb13eed129c9
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-10 20:43:22 +0200

    all dependensies and config in pom.xml were added

--- src/main/java/com/artemnizhnyk/dataanalysergrpcmicroservice/DataAnalyserGrpcMicroserviceApplication.java
+++ src/main/java/com/artemnizhnyk/dataanalysergrpcmicroservice/DataAnalyserGrpcMicroserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DataAnalyserGrpcMicroserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DataAnalyserGrpcMicroserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DataAnalyserGrpcMicroserviceApplication.class, args);
+    }
 
 }
